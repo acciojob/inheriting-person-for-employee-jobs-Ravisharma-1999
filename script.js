4b83d8e821581f261d7d2e1735f83907279fb5be
Ravisharma-1999 <ravi829144@gmail.com>
2024-11-25 08:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class Person {
     }
 
     greet() {
-        return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 }
 
@@ -18,38 +18,10 @@ class Employee extends Person {
     }
 
     jobGreet() {
-        return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
 
-// DOM Elements
-const nameInput = document.getElementById('name');
-const ageInput = document.getElementById('age');
-const jobTitleInput = document.getElementById('jobTitle');
-const outputDiv = document.getElementById('output');
-
-// Event Listeners
-document.getElementById('createPerson').addEventListener('click', () => {
-    const name = nameInput.value;
-    const age = parseInt(ageInput.value);
-
-    if (name && age) {
-        const person = new Person(name, age);
-        outputDiv.innerHTML = `<p>${person.greet()}</p>`;
-    } else {
-        outputDiv.innerHTML = `<p>Please enter both name and age for the person.</p>`;
-    }
-});
-
-document.getElementById('createEmployee').addEventListener('click', () => {
-    const name = nameInput.value;
-    const age = parseInt(ageInput.value);
-    const jobTitle = jobTitleInput.value;
-
-    if (name && age && jobTitle) {
-        const employee = new Employee(name, age, jobTitle);
-        outputDiv.innerHTML = `<p>${employee.jobGreet()}</p>`;
-    } else {
-        outputDiv.innerHTML = `<p>Please enter name, age, and job title for the employee.</p>`;
-    }
-});
+// Attach classes to the global window object for Cypress tests
+window.Person = Person;
+window.Employee = Employee;
