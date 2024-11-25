7e0ce45ef6ffbfddc3f104e415346e8ac304af36
Ravisharma-1999 <ravi829144@gmail.com>
2024-11-25 08:38:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
-// complete this js code
-function Person(name, age) {}
+// Person Class
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-function Employee(name, age, jobTitle) {}
+    greet() {
+        return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
+    }
+}
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+// Employee Class
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); // Call the constructor of the Person class
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
+    }
+}
+
+// DOM Elements
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const jobTitleInput = document.getElementById('jobTitle');
+const outputDiv = document.getElementById('output');
+
+// Event Listeners
+document.getElementById('createPerson').addEventListener('click', () => {
+    const name = nameInput.value;
+    const age = parseInt(ageInput.value);
+
+    if (name && age) {
+        const person = new Person(name, age);
+        outputDiv.innerHTML = `<p>${person.greet()}</p>`;
+    } else {
+        outputDiv.innerHTML = `<p>Please enter both name and age for the person.</p>`;
+    }
+});
+
+document.getElementById('createEmployee').addEventListener('click', () => {
+    const name = nameInput.value;
+    const age = parseInt(ageInput.value);
+    const jobTitle = jobTitleInput.value;
+
+    if (name && age && jobTitle) {
+        const employee = new Employee(name, age, jobTitle);
+        outputDiv.innerHTML = `<p>${employee.jobGreet()}</p>`;
+    } else {
+        outputDiv.innerHTML = `<p>Please enter name, age, and job title for the employee.</p>`;
+    }
+});
